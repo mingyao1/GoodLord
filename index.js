8f05cdd1e682df3a43e79cb786796d40a919f547
mingyao1 <106688909+mingyao1@users.noreply.github.com>
2023-10-04 16:30:39 -0500

    post

--- index.js
+++ index.js
@@ -3,10 +3,20 @@ const axios = require('axios');
 const app = express();
 const port = 3000;
 
-app.get('/search', async (req, res) => {
+app.use(express.json())
+
+app.post('/search', async (req, res) => {
   try {
-    const query = req.query.q; // Get search query from request
+    const query = req.body.query; // Get search query from request
+    if (!query) {
+      res.status = 401
+      res.json({ error: 'Bro put a query' })
+      console.log('Bro put a query')
+      return
+    }
     const apiKey = process.env.API_KEY; // Replace with your actual API key
+
+    console.log(`query: ${query}`)
     
     const config = {
       headers: {
@@ -15,10 +25,11 @@ app.get('/search', async (req, res) => {
     };
     
     const apiRes = await axios.get(`https://api.scripture.api.bible/v1/bibles/de4e12af7f28f599-01/search?query=${query}`, config);
-    
+    console.log(apiRes.data)
     res.json(apiRes.data); // Send response back to client
   } catch (error) {
     res.status(500).json({ error: 'Something went wrong' });
+    console.log('Something went wrong\n', error)
   }
 });
 
